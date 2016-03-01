8fd2aa1252073ad3ece6ff14d94ce988c4e25399
Andreas Beasley <andreas@sapioweb.com>
2016-03-01 10:53:52 -0800

    facebook tracking code to head

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -21,3 +21,19 @@
 <meta name="msapplication-navbutton-color" content="#37474F">
 <!-- iOS Safari -->
 <meta name="apple-mobile-web-app-status-bar-style" content="#37474F">
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '798445970260409');
+fbq('track', 'ViewContent');
+fbq('track', "PageView");
+</script>
+
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=798445970260409&ev=PageView&noscript=1"/></noscript>
+<!-- End Facebook Pixel Code -->
