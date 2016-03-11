a1883d92447a16a30ad399c7d6030874b4fc1c09
Andreas Beasley <andreas@sapioweb.com>
2016-03-11 13:22:05 -0800

    markdown providers implemented into app config

--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Haleks\Markdown\MarkdownServiceProvider::class,
 
     ],
 
@@ -201,6 +202,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Markdown' => Haleks\Markdown\Facades\Markdown::class,
+
 
     ],
 
