0fcc6dc8de1fbfc4203969f1ad5818bb5cf45575
Andreas Beasley <andreas@sapioweb.com>
2016-03-11 13:21:05 -0800

    github data integrated into controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,19 +8,44 @@ use App\Http\Requests;
 
 class PagesController extends Controller
 {
-   	public function showWelcome() {
+   	public function showWelcome()
+   	{
     		return view('welcome');
 	}
 
-	public function showContact() {
+	public function showContact()
+	{
     		return view('contact');
 	}
 
-	public function showBlog() {
-		abort(503);
+	public function showBlog()
+	{
+		$client = new \GuzzleHttp\Client();
+
+		$gists = $client->request('GET', env('GITHUB_API_URL') . '/users/sapiobeasley/gists');
+
+		$gists = json_decode($gists->getBody());
+
+		return view('blog')->with([
+			'gists' => $gists
+		]);
 	}
 
-	public function showPortfolio () {
+	public function showSingleBlog($id)
+	{
+		$client = new \GuzzleHttp\Client();
+
+		$gist = $client->request('GET', env('GITHUB_API_URL') . '/gists/' .  $id);
+
+		$gist = json_decode($gist->getBody());
+
+		return view('blog-single')->with([
+			'gist' => $gist
+		]);
+	}
+
+	public function showPortfolio ()
+	{
 		abort(503);
 	}
 }
