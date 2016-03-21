f48aa7d099172eabad5ef15aa6ec9dccd66cb1b2
Andreas Beasley <andreas@sapioweb.com>
2016-03-21 00:33:43 -0700

    form and email templates added, and contact pages using form include

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,30 +11,7 @@
 			<section class=jumbotron>
 				<div class='bg animated' style='background:#fff;'></div>
 				<div class='row vcenter'>
-					<form action='#' class=row id=form name=form>
-						<div class='col-md-8 col-md-offset-2'>
-							<div class=block>
-								<div class='title-block text-center'>
-									<h3>Contact Us</h3>
-								</div>
-								<div class=row>
-									<div class=col-md-6>
-										<input class=form-control id=name name=name placeholder=Name>
-										<input class=form-control id=email name=email placeholder=E-mail>
-										<input class=form-control id=phone name=phone placeholder=Phone>
-									</div>
-									<div class=col-md-6>
-										<textarea class=form-control id=message name=message placeholder=Message type=text></textarea>
-									</div>
-								</div>
-								<div class=row>
-									<div class='col-md-12 text-center'>
-										<button class=btn id=send type=submit>Send</button>
-									</div>
-								</div>
-							</div>
-						</div>
-					</form>
+					@include('forms.contact')
 				</div>
 			</section>
 
