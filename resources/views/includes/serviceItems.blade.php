847a3dc2e66ff2069445605678854dd87e0d5d9d
Andreas Beasley <andreas.beasley@gmail.com>
2016-07-07 09:48:27 -0700

    service learn more links removed for now

--- resources/views/includes/serviceItems.blade.php
+++ resources/views/includes/serviceItems.blade.php
@@ -4,7 +4,7 @@
         <span class="linea-basic-pencil-ruler"></span>
         <h4>Design &amp; Development</h4>
         <p>Our team is well versed in a plethora of developement languages. Only the best will be utilized for your next great website.</p>
-        <a href="{{route('services.development')}}">Learn more <span class="linea-arrows-slim-right"></span></a>
+        {{-- <a href="{{route('services.development')}}">Learn more <span class="linea-arrows-slim-right"></span></a> --}}
     </div>
 </div>
 
@@ -14,7 +14,7 @@
         <span class="linea-basic-server-cloud"></span>
         <h4>Hosting</h4>
         <p>Web Hosting is the most important aspect of any website. We have hosting services from shared to full dedicated servers.</p>
-        <a href="{{route('services.hosting')}}">Learn more <span class="linea-arrows-slim-right"></span></a>
+        {{-- <a href="{{route('services.hosting')}}">Learn more <span class="linea-arrows-slim-right"></span></a> --}}
     </div>
 </div>
 
@@ -24,6 +24,6 @@
         <span class="linea-basic-headset"></span>
         <h4>Maintinance &amp; Support</h4>
         <p>If your website is over 2 years old it may be out of date. It's important that you reflect the “latest and greatest”.</p>
-        <a href="{{route('services.maintinance')}}">Learn more <span class="linea-arrows-slim-right"></span></a>
+        {{-- <a href="{{route('services.maintinance')}}">Learn more <span class="linea-arrows-slim-right"></span></a> --}}
     </div>
 </div>
