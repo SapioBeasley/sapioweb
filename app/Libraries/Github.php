81536c1153371f106c2c3b10f255a6bff00cecd8
Andreas Beasley <andreas.beasley@gmail.com>
2016-07-28 09:37:28 -0700

    if blog id does not exist in gists then abort 404.

--- app/Libraries/Github.php
+++ app/Libraries/Github.php
@@ -4,30 +4,28 @@ namespace App\Libraries;
 
 class Github
 {
+  public function getGists()
+  {
+    return $this->getGistsTransform('GET', env('GITHUB_API_URL') . '/users/sapiobeasley/gists');
+  }
 
-    public function __construct()
-    {
-        # code...
+  public function getSingleGist($id)
+  {
+    return $this->getGistsTransform('GET', env('GITHUB_API_URL') . '/gists/' .  $id);
+  }
+
+  public function getGistsTransform($method, $uri)
+  {
+    $client = new \GuzzleHttp\Client();
+
+    try {
+      $gists = $client->request($method, $uri);
+    } catch (\GuzzleHttp\Exception\ClientException $e) {
+      return response()->json($e->getMessage(), $e->getCode());
     }
 
-    public function getGists()
-    {
-        return $this->getGistsTransform('GET', env('GITHUB_API_URL') . '/users/sapiobeasley/gists');
-    }
+    $gists = json_decode($gists->getBody());
 
-    public function getSingleGist($id)
-    {
-		return $this->getGistsTransform('GET', env('GITHUB_API_URL') . '/gists/' .  $id);
-    }
-
-    public function getGistsTransform($method, $uri)
-	{
-		$client = new \GuzzleHttp\Client();
-
-		$gists = $client->request($method, $uri);
-
-		$gists = json_decode($gists->getBody());
-
-		return $gists;
-	}
+    return $gists;
+  }
 }
